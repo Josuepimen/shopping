3d566f3c24fbe8f92f5241718232800763cd3d38
Josue Pimentel <Josuedavidpimentel05@gmail.com>
2024-12-13 10:00:19 -0400

    actualizacion de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import data from '../data.json';
 
-
 function App() {
   return (
     <div className='mx-auto p-4'>
@@ -9,11 +8,15 @@ function App() {
       <div className='flex flex-col items-center gap-4'>
         {data.map((dessert, index) => (
           <div key={index} className='border rounded-lg p-4 shadow-lg w-full max-w-xs'>
-            <img 
-              src={dessert.image.thumbnail} 
-              alt={dessert.name} 
-              className='w-full h-auto rounded-md'
-            />
+            <picture>
+              <source media="(max-width: 639px)" srcSet={dessert.image.mobile} />
+              <source media="(min-width: 640px) and (max-width: 1023px)" srcSet={dessert.image.tablet} />
+              <img 
+                src={dessert.image.desktop} 
+                alt={dessert.name} 
+                className='h-full rounded-md'
+              />
+            </picture>
             <h2 className='font-bold text-lg mt-2 text-center'>{dessert.name}</h2>
             <p className='text-gray-700 text-center'>{dessert.category}</p>
             <p className='text-orange-600 font-semibold text-center'>${dessert.price.toFixed(2)}</p>
